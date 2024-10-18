9ef011c053ca14bf30b1ea9b2b2f9388778f957f
Pooji A <p.annabathul@student.uw.edu.pl>
2024-10-18 22:58:49 +0200

    fullproblemset

--- ProblemSets/ps1.jl
+++ ProblemSets/ps1.jl
@@ -56,8 +56,8 @@ function count_positives(arr)
     return counter
 end
 
-count_positives([1, -3, 4, 7, -2, 0])
-count_positives([-5, -10, 0, 6]) 
+println(count_positives([1, -3, 4, 7, -2, 0]))
+println(count_positives([-5, -10, 0, 6]))
 
 
 
@@ -67,7 +67,46 @@ function plot_powers(n)
         pow(x) = x^i
         plot!(pow,-10:0.1:10)
     end
-    plot!(legend=true)
+    p=plot!(legend=true)
+    savefig(p,"plot_powers_output.png")
 end    
 
 plot_powers(2)
+
+
+function standard_deviation(x)
+    mu_hat = mean(x)
+    d = x.- mu_hat
+    squared_d = d.^ 2
+    variance = sum(squared_d)/(length(x) - 1)
+    return sqrt(variance)
+end
+
+println(standard_deviation([1,2,3,4,5]))
+println(standard_deviation([5,10,15]))
+println(standard_deviation(collect(2:7)))
+
+
+data = readdlm("dataset.txt", '\t', Float64)
+
+#println(data)
+
+earnings = data[:,1]
+education = data[:,2]
+hours_worked = data[:,3]
+
+p1=scatter(education, earnings, xlabel="Education", ylabel="Earnings", title="Relationship between education and earnings",color = "green")
+p2=scatter(hours_worked, earnings, xlabel="Hours Worked", ylabel="Earnings", title="Relationship between hours worked and earnings",color = "red")
+
+plot(p1)
+savefig(p1,"education_vs_earnings.png")
+
+plot(p2)
+savefig(p2,"hoursworked_vs_earnings.png")
+
+corr_education_earnings = cor(education, earnings)
+corr_hoursworked_earnings = cor(hours_worked, earnings)
+
+println("Correlation between earnings and education:",corr_education_earnings)
+println("Correlation between hours worked and earnings:",corr_hoursworked_earnings)
+
