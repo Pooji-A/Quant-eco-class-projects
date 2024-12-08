461d20a6850e9cf21b6cadb1da0b845798cf7a64
Sergio Cárcamo <scarcamo@users.noreply.github.com>
2024-12-08 19:50:38 +0100

    add problem 2

--- ProblemSets/ps2.jl
+++ ProblemSets/ps2.jl
@@ -91,3 +91,85 @@ result = iterative_solver(h1, 100.5, 0.5)
 
 
 
+# Problem 2: Linear algebra
+# Ax = b
+
+using LinearAlgebra, Printf, PrettyTables
+
+
+function exact_solution(α, β)
+    # Get exact solution Ax=b by solving system backwards
+
+    # from the equations:
+    x5 = 1.0
+    x4 = x5
+    x3 = x4
+    x2 = x3
+    # From first equation: x1 + (-1)x2 + (α-β)x4 + βx5 = α
+    x1 = α + x2 - (α-β)*x4 - β*x5
+    
+    return [x1, x2, x3, x4, x5]
+end
+
+
+
+
+function solve_system(α, β)
+    # Returns exact solution, numerical solution, condition number, and relative residual.
+    
+    A = Float64[
+        1 -1  0  α-β  β;
+        0  1 -1  0    0;
+        0  0  1 -1    0;
+        0  0  0  1   -1;
+        0  0  0  0    1
+    ]
+    
+    b = Float64[α, 0, 0, 0, 1]
+    
+    # Get exact and numerical solution
+    x_exact = exact_solution(α, β)
+    x_numerical = A \ b
+    
+    # Calculate relative residuals
+    rel_residual = norm(A*x_numerical - b) / norm(b)
+    
+    cond_num = cond(A)
+    
+    return (
+        exact = x_exact,
+        numerical = x_numerical,
+        residual = rel_residual,
+        cond = cond_num
+    )
+end
+
+
+
+# Create analysis table
+
+b  = 0.1
+β_values = Float64[10.0^i for i in 0:12]
+
+x1_exact = Float64[]
+x1_numerical = Float64[]
+cond_numbers = Float64[]
+residuals = Float64[]
+
+for β in β_values
+    result = analyze_system(α, β)
+    push!(x1_exact, result.exact[1])
+    push!(x1_numerical, result.numerical[1])
+    push!(cond_numbers, result.cond)
+    push!(residuals, result.residual)
+end
+
+
+header = ["β", "x1 (exact)", "x1 (numerical)", "cond(A)", "relative residual"]
+data = hcat(β_values, x1_exact, x1_numerical, cond_numbers, residuals)
+
+pretty_table(data;
+    header = header,
+    formatters = (ft_printf("%.0e", [1]), 
+                    ft_printf("%.6f", [2,3]),
+                    ft_printf("%.2e", [4,5])))
